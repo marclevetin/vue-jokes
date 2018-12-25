9836562a222be4b560de7ea2763b9d94717bbf2a
marclevetin <marc@derbyhat.org>
2018-12-24 20:09:41 -0500

    Added vuetify.  Got router basics working

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <span>
+    <v-navigation-drawer app v-model="drawer" class="blue" disable-resize-watcher temporary>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile v-for="item in items" :key="item.title" @click>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <router-link :to="item.url">
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon 
+        class="hidden-md-and-up"
+        @click="drawer = !drawer">
+      </v-toolbar-side-icon>
+      <router-link to="/">
+        <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+      </router-link>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-for="item in items" :key="item.title" :to="item.url">{{item.title}}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      appTitle: "Jokes",
+      drawer: false,
+      items: [
+        { title: "Random Joke", url: "/joke" }, 
+        { title: "Search", url: "/search"},
+        { title: "About", url: "/about"}
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Search.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="about"><h1>Search</h1></div>
+</template>
