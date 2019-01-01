d8f23abcd2d1a0289519ef5fe0c642f756370dc2
marclevetin <marc@derbyhat.org>
2019-01-01 18:22:56 -0500

    Added v-app to App.vue so that all of the built in Vuetify styling would take hold.  Converted Search.vue to use Vuetify List, slightly styled.  Lots of work fiddling complete and successful to disable default HTML submit functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <navbar></navbar>
-    <v-content transition="slide-x-transition">
-      <v-container fluid> <router-view></router-view> </v-container>
-    </v-content>
-    <v-footer app></v-footer>
-  </div>
+  <v-app>
+    <div>
+      <navbar></navbar>
+      <v-content transition="slide-x-transition">
+        <v-container align-center>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </div>
+  </v-app>
 </template>
 
 <script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,25 +1,55 @@
 <template>
   <div>
-  <h1>Search the API</h1>
-  <input placeholder="Enter search term" v-model="searchTerm"/> 
-  <button @click="getJokes(searchTerm)">Submit</button>
-  <ul>
-    <li v-for="(joke, index) in allJokes" :key="index">{{joke.joke}}</li>
-  </ul>
+    <h1>Search the API</h1>
+
+    <v-form ref="form" v-model="valid" lazy-validation @submit.prevent>
+      <v-text-field
+        v-model="searchTerm"
+        :rules="searchRules"
+        label="Search term"
+        @keyup.enter="getJokes(searchTerm)"
+        required
+      ></v-text-field>
+      <v-btn :disabled="!valid" @click="getJokes(searchTerm)" color="success">Submit</v-btn>
+      <v-btn small flat @click="clear">Clear</v-btn>
+    </v-form>
+
+    <v-list v-if="allJokes.length">
+      <v-list-tile v-for="(joke, index) in allJokes" :key="index" @click>
+        <v-list-tile-content>
+          <v-list-tile-title class="list__tile__override">{{joke.joke}}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <v-list v-else="!allJokes.length">
+      <v-list-tile>
+        <v-list-tile-content>
+          <v-list-tile-title>No results found.</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Search',
-  data() {
+  name: "Search",
+  data: () => {
     return {
-      searchTerm: ''
-    }
+      searchTerm: "",
+      searchRules: [v => !!v || "Search term is required"],
+      valid: true
+    };
   },
   methods: {
     getJokes(term) {
-      this.$store.dispatch("getAllJokes", term)
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("getAllJokes", term);
+        this.clear();
+      }
+    },
+    clear() {
+      this.$refs.form.reset();
     }
   },
   computed: {
@@ -27,5 +57,13 @@ export default {
       return this.$store.state.allJokes;
     }
   }
-}
+};
 </script>
+
+<style scoped>
+  .list__tile__override {
+    overflow: visible;
+    white-space: normal;
+    min-height: fit-content;
+  }
+</style>
