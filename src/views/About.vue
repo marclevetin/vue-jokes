1c10d6bfb0e0c2f6d2cb8c6da74ebefa780843d2
marclevetin <marc@derbyhat.org>
2019-01-01 20:17:22 -0500

    Removed Hello World components.  Added a bit of content to About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <div class="about">
+    <h1>Hello World!</h1>
+    <p>This app is a reincarnation of <a href="https://marclevetin.github.io/dad-jokes" rel="noopener" target="_blank">this project.</a>  It is my first attempt at building something in Vue.</p>
+    <p>It pulls data from the Dad Joke API found at <a href="https://icanhazdadjoke.com/" rel="noopener" target="_blank">https://icanhazdadjoke.com/</a></p>
+    <p>Tech includes <a href="https://vuejs.org/" rel="noopener" target="_blank">Vue</a>, <a href="https://vuex.vuejs.org/" rel="noopener" target="_blank">Vuex</a>, <a href="https://router.vuejs.org/" rel="noopener" target="_blank">Vue Router</a>, <a href="https://vuetifyjs.com/en/" rel="noopener" target="_blank">Vuetify</a>, and <a href="https://cli.vuejs.org/" rel="noopener" target="_blank">Vue CLI 3</a>.  
+    There's nothing fancy here.  Just trying to cover the basics.</p>
+    <h2>Why Vue?</h2>
+    <p>Simple, really.  I'd never worked with it before.  Some nice folks at a nh.js meetup have demonstrated some Vue projects.  I thought "neat" and here we are.</p>
+    <p>Feedback welcome.</p>
+  </div>
 </template>
